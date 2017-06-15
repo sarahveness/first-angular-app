9329df6a60531f5e0d03e42b81ae706adf61b93c
Sarah Veness <sarahveness@omnifilm.com>
2017-06-15 14:20:08 -0700

    put shop into its own class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My Very First Angular App';
+  title = 'Gastown Cofeee';
+  shop: Shop = {
+    id: 1,
+    name: 'Prado Cafe'
+  };
+}
+
+export class Shop {
+  id: number;
+  name: string;
 }
